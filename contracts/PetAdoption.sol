94dda46a9ae3b45b6e37c9c4dcf29498a785ad54
leo <nascimentoleo899@gmail.com>
2024-01-28 17:34:06 -0300

    Add pet functionality and owner modifier

--- contracts/PetAdoption.sol
+++ contracts/PetAdoption.sol
@@ -3,9 +3,19 @@ pragma solidity ^0.8.9;
 
 contract PetAdoption {
   address public owner;
+  uint public petIndex = 0;
 
   constructor() {
     owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner, "Only the owner can call this function.");
+    _;
+  }
+
+  function addPet() public onlyOwner {
+   petIndex++;
   }
 
   function getOwner() public view returns (address) {
--- test/PetAdoption.ts
+++ test/PetAdoption.ts
@@ -6,11 +6,11 @@ import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 describe("PetAdoption", function() {
 
    async function deployContractFixture() {
-      const [owner] = await ethers.getSigners();
+      const [owner, account2] = await ethers.getSigners();
       const PetAdoption = await ethers.getContractFactory("PetAdoption");
       const contract = await PetAdoption.deploy();
       await contract.waitForDeployment();
-      return {owner, contract};
+      return {owner, contract, account2};
    }
 
    describe("Deployment", function() {
@@ -24,6 +24,21 @@ describe("PetAdoption", function() {
          const {owner, contract} = await loadFixture(deployContractFixture);
          const contractOwner = await contract.getOwner();
          expect(contractOwner).to.equal(owner.address);
+      });
+   });
+
+   describe("Add Pet", function() {
+      it("Should add a pet", async function() {
+         const {owner, contract} = await loadFixture(deployContractFixture);
+         await contract.addPet();
+         const petCount = await contract.petIndex();
+         expect(petCount).to.equal(1);
+      });
+
+      it("Should revert with the right error in case of other account", async function() {
+         const {owner, contract, account2} = await loadFixture(deployContractFixture);
+         await expect(contract.connect(account2).addPet())
+            .to.be.revertedWith("Only the owner can call this function.");
       });
    });
  });
